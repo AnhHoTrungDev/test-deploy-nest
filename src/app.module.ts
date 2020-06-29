4617dde18ff591af5ed0aca5e1b2602e2d561dd0
Anh <hotrunganhht1912@gmail.com>
2020-06-29 17:07:57 +0700

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,18 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       playground: true,
     }),
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url:
-        'mongodb+srv://anh:191296@cluster0-hfcyz.mongodb.net/test?retryWrites=true&w=majority',
+      url: `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0-hfcyz.mongodb.net/test?retryWrites=true&w=majority`,
       entities: [join(__dirname, '**/**.entity{.ts,.js}')],
       synchronize: true,
       useNewUrlParser: true,
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(port);
-  Logger.log(`🚀 Server running on http://localhost:${port}`, 'Bootstrap');
+  Logger.log(`🚀 Server running on http://localhost:${port} 🚀 `, 'Bootstrap');
 }
 bootstrap();
